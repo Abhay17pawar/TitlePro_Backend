fe58d36cde6cb5090f3fec1f70ce79b425b1091b
Abhay Pawar <abhaypawar0817@gmail.com>
2025-03-17 13:19:55 +0530

    changed cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const pool = require("./server/config/database"); 
+const pool = require("./server/config/database");
 const userRouter = require("./server/router/route");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -8,27 +8,20 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
 
-const allowedOrigins = ['https://luxury-brigadeiros-40458b.netlify.app'];
-
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
+
+// CORS configuration to allow any origin
 const corsOptions = {
-    origin: function (origin, callback) {
-      // Check if the origin is in the allowedOrigins array
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);  // Allow the request
-      } else {
-        callback(new Error('Not allowed by CORS'));  // Reject the request
-      }
-    },
+    origin: "*",  // Allow requests from any origin
     methods: 'GET, POST, PUT, DELETE',
-    credentials: true, // If you need to send cookies or authentication headers
-  };
-  
-  // Use the CORS middleware with the options
-  app.use(cors(corsOptions));
-  
+    credentials: true,  // If you need to send cookies or authentication headers
+};
+
+// Use the CORS middleware with the options
+app.use(cors(corsOptions));
+
 // Database Connection Test
 pool.connect()
     .then(() => console.log("✅ PostgreSQL Database Connected Successfully"))
@@ -42,7 +35,6 @@ app.use((err, req, res, next) => {
     console.error("Error:", err.message);
     res.status(500).json({ status: 500, message: "Internal Server Error" });
 });
-
 
 // Start Server
 app.listen(PORT, () => {
