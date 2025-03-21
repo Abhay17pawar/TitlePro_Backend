0194239b1c48809dc59447bbb2359873df008720
Abhay Pawar <abhaypawar0817@gmail.com>
2025-03-21 22:48:12 +0530

    fix sequelize error

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -1,13 +1,26 @@
 const Sequelize = require('sequelize');
-const { DB_USER, DB_HOST, DB_PASSWORD, DB_NAME } = require('./server_config');
+const { DB_USER, DB_PORT, DB_HOST, DB_PASSWORD, DB_NAME } = require('./server_config');
 
 
 
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    dialect:'postgres', 
-});
-
+    port: DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, // Enable SSL for remote database
+        rejectUnauthorized: false, // Allow self-signed certificates
+      },
+    },
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000, // Increase connection acquire timeout (30 seconds)
+      idle: 10000, // Increase idle timeout (10 seconds)
+    },
+  });
+  
 
 module.exports = sequelize;
