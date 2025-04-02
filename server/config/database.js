d88e314932ad3b91c9aa1ac401ee3b920eef305b
Abhay Pawar <abhaypawar0817@gmail.com>
2025-04-02 13:16:19 +0530

    changed ssl

--- server/config/database.js
+++ server/config/database.js
@@ -8,11 +8,11 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD, 
   port: process.env.DB_PORT, 
-  // ssl: {
-  //   rejectUnauthorized: false, // This is required for self-signed certificates
+   ssl: {
+    rejectUnauthorized: false, // This is required for self-signed certificates
   //   // Additional SSL configuration options, depending on your setup
   //   ca: process.env.DB_CA_CERT || null, // If you need to specify a CA certificate
-  // },
+   },
 });
 
 // pool.connect()
